f5df9c69e5e0755c67a2d2ea61f0377a1d6bd20b
itsjipa <nazhiifahn@gmail.com>
2024-09-29 18:21:02 +0700

    add home

---
+++ src/home/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import NavbarApp from "../NavbarApp";
+
+const Home = () => {
+    return (
+        <>
+        <NavbarApp />
+        <Container>
+
+        </Container>
+        </>
+    );
+};
+
+export default Home;
