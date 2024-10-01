d22d6c726879b85199ada0ba6522cf3849f6430e
itsjipa <nazhiifahn@gmail.com>
2024-10-01 23:28:19 +0700

    update home menu

--- src/home/home.js
+++ src/home/home.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import NavbarApp from "../NavbarApp";
+import CarouselSlider from "./CarouselSlider";
+import '../styling/Home.css'
+import PopularMenu from "./PopularMenu";
 
 const Home = () => {
     return (
         <>
         <NavbarApp />
-        <Container>
-
+        <Container fluid className="p-3 px-5 contain">
+            <CarouselSlider />
         </Container>
+        <PopularMenu />
         </>
     );
 };
